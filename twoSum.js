81d6d5cf6b6a38ad6bdfdfc474582584b657adba
Anh Vu <vu.haianh291@gmail.com>
2021-03-07 14:21:19 +0200

    Feat: Two sum solution

---
+++ twoSum.js
@@ -0,0 +1,16 @@
+function twoSum(nums, target) {
+    const numDict = nums.map((num, i) => ({
+        value: num,
+        index: i
+    }))
+    numDict.sort((a, b) => a.value - b.value)
+    let leftPoint = 0
+    let rightPoint = nums.length - 1
+    while (leftPoint <= rightPoint) {
+        const sum = numDict[leftPoint].value + numDict[rightPoint].value
+        if (sum === target) break
+        else if (sum < target) leftPoint++
+        else rightPoint--
+    }
+    return [numDict[leftPoint].index, numDict[rightPoint].index]
+}
